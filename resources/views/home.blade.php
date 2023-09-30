eded7b4f65e591919e209aef386bee90bd053dbb
0724yuko <y45285hbkh@gmail.com>
2023-09-30 19:02:00 +0900

    20230930

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,6 +9,7 @@
 @section('content')
 
     <div class="container">
+        <img class="d-block mx-auto img-fluid" src="/image/HOMEsm.png" alt="HOME画像">
 
         @csrf
 
@@ -19,20 +20,37 @@
                 </div>
             </div>
         </div> -->
+    <div class="d-flex justify-content-center align-items-center">
         <div class="card-tools">
-            <div class="input-group input-group-sm">
+            <div class="input-group input-group-lg">
+                <div class="input-group-append">
+                    <a href="{{ url('items/') }}" class="btn btn-default">対応一覧へ</a>
+                </div>
+            </div>
+        </div>
+        <div class="card-tools">
+            <div class="input-group input-group-lg">
                 <div class="input-group-append">
                     <a href="{{ url('clients') }}" class="btn btn-default">顧客一覧へ</a>
                 </div>
             </div>
         </div>
         <div class="card-tools">
-            <div class="input-group input-group-sm">
+            <div class="input-group input-group-lg">
                 <div class="input-group-append">
-                    <a href="{{ url('items/') }}" class="btn btn-default">対応一覧へ</a>
+                    <a href="{{ url('users') }}" class="btn btn-default">担当者一覧へ</a>
                 </div>
             </div>
         </div>
+        <div class="card-tools">
+            <div class="input-group input-group-lg">
+                <div class="input-group-append">
+                    <a href="{{ url('items/add') }}" class="btn btn-default">新規対応登録へ</a>
+                </div>
+            </div>
+        </div>
+        
+    </div>
 
 </div> 
 
